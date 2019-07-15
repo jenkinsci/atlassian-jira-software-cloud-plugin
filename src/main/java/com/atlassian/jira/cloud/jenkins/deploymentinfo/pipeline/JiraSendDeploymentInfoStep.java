590af16784e2507fa1ea1b3058119e80f06bc385
Lenin <lrajasekaran@atlassian.com>
2019-07-15 12:44:24 +1000

    JENKINS-58416 Display name for deployment step

--- src/main/java/com/atlassian/jira/cloud/jenkins/deploymentinfo/pipeline/JiraSendDeploymentInfoStep.java
+++ src/main/java/com/atlassian/jira/cloud/jenkins/deploymentinfo/pipeline/JiraSendDeploymentInfoStep.java
@@ -1,12 +1,16 @@
 package com.atlassian.jira.cloud.jenkins.deploymentinfo.pipeline;
 
+import com.atlassian.jira.cloud.jenkins.Messages;
 import com.atlassian.jira.cloud.jenkins.common.factory.JiraSenderFactory;
 import com.atlassian.jira.cloud.jenkins.common.response.JiraSendInfoResponse;
+import com.atlassian.jira.cloud.jenkins.config.JiraCloudPluginConfig;
+import com.atlassian.jira.cloud.jenkins.config.JiraCloudSiteConfig;
 import com.atlassian.jira.cloud.jenkins.deploymentinfo.service.JiraDeploymentInfoRequest;
 import com.google.common.collect.ImmutableSet;
 import hudson.Extension;
 import hudson.model.Run;
 import hudson.model.TaskListener;
+import hudson.util.ListBoxModel;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.steps.Step;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
@@ -16,10 +20,18 @@ import org.jenkinsci.plugins.workflow.steps.SynchronousNonBlockingStepExecution;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
+import javax.inject.Inject;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Set;
 
+/**
+ * Implementation of the "jiraSendDeploymentInfo" step that can be used in Jenkinsfile to send
+ * deployment updates to a Jira site.
+ */
 public class JiraSendDeploymentInfoStep extends Step implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private String site;
     private String environment;
@@ -68,6 +80,8 @@ public class JiraSendDeploymentInfoStep extends Step implements Serializable {
     @Extension
     public static class DescriptorImpl extends StepDescriptor {
 
+        @Inject private transient JiraCloudPluginConfig globalConfig;
+
         @Override
         public Set<? extends Class<?>> getRequiredContext() {
             return ImmutableSet.of(TaskListener.class, Run.class);
@@ -76,6 +90,22 @@ public class JiraSendDeploymentInfoStep extends Step implements Serializable {
         @Override
         public String getFunctionName() {
             return "jiraSendDeploymentInfo";
+        }
+
+        @Override
+        public String getDisplayName() {
+            return Messages.JiraSendDeploymentInfoStep_DescriptorImpl_DisplayName();
+        }
+
+        @SuppressWarnings("unused")
+        public ListBoxModel doFillSiteItems() {
+            ListBoxModel items = new ListBoxModel();
+            final List<JiraCloudSiteConfig> siteList = globalConfig.getSites();
+            for (JiraCloudSiteConfig siteConfig : siteList) {
+                items.add(siteConfig.getSite(), siteConfig.getSite());
+            }
+
+            return items;
         }
     }
 
@@ -93,13 +123,13 @@ public class JiraSendDeploymentInfoStep extends Step implements Serializable {
         @Override
         protected JiraSendInfoResponse run() throws Exception {
             final TaskListener taskListener = getContext().get(TaskListener.class);
-            final WorkflowRun build = (WorkflowRun) getContext().get(Run.class);
+            final WorkflowRun workflowRun = (WorkflowRun) getContext().get(Run.class);
             final JiraDeploymentInfoRequest request =
                     new JiraDeploymentInfoRequest(
                             step.getSite(),
                             step.getEnvironment(),
                             step.getEnvironmentType(),
-                            build);
+                            workflowRun);
             final JiraSendInfoResponse response =
                     JiraSenderFactory.getInstance()
                             .getJiraDeploymentInfoSender()
