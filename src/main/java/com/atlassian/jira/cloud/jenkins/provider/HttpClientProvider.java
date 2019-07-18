12ed0d4d2d5dd8ca237c8301bc33e99e4114017e
Lenin <lrajasekaran@atlassian.com>
2019-07-19 09:49:46 +1000

    JENKINS-58544 Retry 3 times on 5XX from Jira API

--- src/main/java/com/atlassian/jira/cloud/jenkins/provider/HttpClientProvider.java
+++ src/main/java/com/atlassian/jira/cloud/jenkins/provider/HttpClientProvider.java
@@ -1,23 +1,69 @@
 package com.atlassian.jira.cloud.jenkins.provider;
 
 import com.google.inject.Provides;
+import okhttp3.Interceptor;
+import okhttp3.Interceptor.Chain;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.time.Duration;
 
-/**
- * OkHttpClient with appropriate default timeouts
- */
+/** OkHttpClient with appropriate default timeouts */
 public class HttpClientProvider {
 
+    private static final Logger log = LoggerFactory.getLogger(HttpClientProvider.class);
     private final OkHttpClient httpClient;
 
     public HttpClientProvider() {
-        httpClient = new OkHttpClient.Builder()
-                .connectTimeout(Duration.ofMillis(5000))
-                .readTimeout(Duration.ofMillis(5000))
-                .writeTimeout(Duration.ofMillis(5000))
-                .build();
+        httpClient =
+                new OkHttpClient.Builder()
+                        .connectTimeout(Duration.ofMillis(5000))
+                        .readTimeout(Duration.ofMillis(5000))
+                        .writeTimeout(Duration.ofMillis(5000))
+                        .addInterceptor(retryInterceptor())
+                        .build();
+    }
+
+    private Interceptor retryInterceptor() {
+        return chain -> {
+            Request request = chain.request();
+            Response response = chain.proceed(request);
+            response = performRetry(chain, request, response);
+
+            return response;
+        };
+    }
+
+    private Response performRetry(
+            final Chain chain, final Request request, final Response originalResponse)
+            throws IOException {
+        Response response = originalResponse;
+        final int MAX_RETRIES = 3;
+        int currentAttempt = 1;
+
+        while (response.code() >= 500 && currentAttempt <= MAX_RETRIES) {
+            log.warn(
+                    String.format(
+                            "Received %d for request to %s. Retry attempt %d of %d.",
+                            response.code(),
+                            response.request().url(),
+                            currentAttempt,
+                            MAX_RETRIES));
+            response.close();
+            try {
+                Thread.sleep(2000); // delay between each retry
+            } catch (InterruptedException e) {
+                log.error("Retry delay interrupted: " + e.getMessage());
+            }
+            response = chain.proceed(request);
+            currentAttempt++;
+        }
+
+        return response;
     }
 
     @Provides
---
+++ src/test/java/com/atlassian/jira/cloud/jenkins/provider/HttpClientProviderTest.java
@@ -0,0 +1,83 @@
+package com.atlassian.jira.cloud.jenkins.provider;
+
+import com.atlassian.jira.cloud.jenkins.BaseMockServerTest;
+import com.atlassian.jira.cloud.jenkins.HttpClientProviderTestGenerator;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HttpClientProviderTest extends BaseMockServerTest {
+
+    @Inject
+    private OkHttpClient httpClient;
+
+    @Before
+    public void setup() throws IOException {
+        super.setup();
+        httpClient = new HttpClientProvider().httpClient();
+    }
+
+    @Test
+    public void testNoRetryFor2XX() throws IOException {
+        // setup
+        HttpClientProviderTestGenerator.succeedWith2XXOnInitialAttempt(this);
+        final Request request = getRequest();
+
+        // execute
+        final Response response = httpClient.newCall(request).execute();
+
+        // verify
+        assertThat(response.code()).isEqualTo(202);
+    }
+
+    @Test
+    public void testNoRetryFor4XX() throws IOException {
+        // setup
+        HttpClientProviderTestGenerator.failWith4XXOnInitialAttempt(this);
+        final Request request = getRequest();
+
+        // execute
+        final Response response = httpClient.newCall(request).execute();
+
+        // verify
+        assertThat(response.code()).isEqualTo(404);
+    }
+
+    @Test
+    public void testRetryOnceFor5XXAndSucceed() throws IOException {
+        // setup
+        HttpClientProviderTestGenerator.failWith503AndThenSucceed2XX(this);
+        final Request request = getRequest();
+
+        // execute
+        final Response response = httpClient.newCall(request).execute();
+
+        // verify
+        assertThat(response.code()).isEqualTo(202);
+    }
+
+    @Test
+    public void testFailFor5XXAfterThreeRetries() throws IOException {
+        // setup
+        HttpClientProviderTestGenerator.failWith503ForAllAttempts(this);
+        final Request request = getRequest();
+
+        // execute
+        final Response response = httpClient.newCall(request).execute();
+
+        // verify
+        assertThat(response.code()).isEqualTo(503);
+    }
+
+    private Request getRequest() {
+        return new Request.Builder().url(server.url("/test")).build();
+    }
+
+}
