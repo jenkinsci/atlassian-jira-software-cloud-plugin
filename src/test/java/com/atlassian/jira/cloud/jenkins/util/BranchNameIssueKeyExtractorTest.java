6c49db7f89cde22ee28041a77615c6a992271b0f
Tom Hombergs <tom.hombergs@gmail.com>
2022-09-14 17:17:24 +1000

    ARC-1510: read issue keys from branch name env vars

--- src/test/java/com/atlassian/jira/cloud/jenkins/util/BranchNameIssueKeyExtractorTest.java
+++ src/test/java/com/atlassian/jira/cloud/jenkins/util/BranchNameIssueKeyExtractorTest.java
@@ -10,6 +10,7 @@ import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,14 +36,23 @@ public class BranchNameIssueKeyExtractorTest {
         final SCMRevisionAction scmRevisionAction =
                 new SCMRevisionAction(new GitSCMSource(""), new GitBranchSCMRevision(head, ""));
         when(mockWorkflowRun.getAction(SCMRevisionAction.class)).thenReturn(scmRevisionAction);
+        when(mockWorkflowRun.getEnvVars())
+                .thenReturn(
+                        new HashMap<String, String>() {
+                            {
+                                put("BRANCH_NAME", "DEP-56");
+                                put("CHANGE_BRANCH", "DEP-57");
+                            }
+                        });
 
         // when
         final Set<String> issueKeys =
                 classUnderTest.extractIssueKeys(mockWorkflowRun, PipelineLogger.noopInstance());
 
         // then
-        assertThat(issueKeys).containsExactlyInAnyOrder("TEST-123");
+        assertThat(issueKeys).containsExactlyInAnyOrder("TEST-123", "DEP-56", "DEP-57");
     }
+
 
     @Test
     public void testExtractIssueKeys_whenScmRevisionActionNull() {
