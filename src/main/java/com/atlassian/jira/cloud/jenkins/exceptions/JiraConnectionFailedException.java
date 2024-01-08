b0b6c463a2ef570a8f1cf7040e529bf7e6f813c1
Josh Kay <jkay@atlassian.com>
2024-01-09 12:08:31 +1300

    overload exceptions

--- src/main/java/com/atlassian/jira/cloud/jenkins/exceptions/JiraConnectionFailedException.java
+++ src/main/java/com/atlassian/jira/cloud/jenkins/exceptions/JiraConnectionFailedException.java
@@ -4,4 +4,12 @@ public class JiraConnectionFailedException extends Exception {
     public JiraConnectionFailedException(final String message) {
         super(message);
     }
+
+    public JiraConnectionFailedException(final Throwable cause) {
+        super(cause);
+    }
+
+    public JiraConnectionFailedException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 }
