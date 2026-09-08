2a7c3c1afd4daed96c8f4558fe2f5d14902fc506
Abhirama <abhiram.b@icloud.com>
2026-09-08 16:51:21 +0530

    Add JenkinsRule integration tests for RunUrlProvider verifying root URL path

--- src/test/java/com/atlassian/jira/cloud/jenkins/util/RunUrlProviderTest.java
+++ src/test/java/com/atlassian/jira/cloud/jenkins/util/RunUrlProviderTest.java
@@ -1,47 +1,55 @@
 package com.atlassian.jira.cloud.jenkins.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
 import hudson.model.Job;
 import hudson.model.Run;
 import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper;
+import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 
 public class RunUrlProviderTest {
 
+    @Rule public JenkinsRule jRule = new JenkinsRule();
+
     @Test
-    public void testGetRunUrl_fallsBackToAbsoluteUrlWhenJenkinsNull() throws Exception {
+    public void testGetRunUrl_withJenkinsRootUrl() throws Exception {
+        final FreeStyleProject project = jRule.createFreeStyleProject("test-project");
+        final FreeStyleBuild build = jRule.buildAndAssertSuccess(project);
+
         final RunWrapper runWrapper = mock(RunWrapper.class);
-        when(runWrapper.getAbsoluteUrl())
-                .thenReturn("http://localhost:8080/job/test-repo/job/bugfix%252FSAL-1/1/");
+        doReturn(build).when(runWrapper).getRawBuild();
 
         final String url = RunUrlProvider.getRunUrl(runWrapper);
 
-        assertThat(url).isEqualTo("http://localhost:8080/job/test-repo/job/bugfix%252FSAL-1/1/");
+        final String expectedUrl = jRule.jenkins.getRootUrl() + build.getUrl();
+        assertThat(url).isEqualTo(expectedUrl);
+    }
+
+    @Test
+    public void testGetPipelineUrl_withJenkinsRootUrl() throws Exception {
+        final FreeStyleProject project = jRule.createFreeStyleProject("test-pipeline");
+        final FreeStyleBuild build = jRule.buildAndAssertSuccess(project);
+
+        final RunWrapper runWrapper = mock(RunWrapper.class);
+        doReturn(build).when(runWrapper).getRawBuild();
+
+        final String url = RunUrlProvider.getPipelineUrl(runWrapper);
+
+        final String expectedUrl = jRule.jenkins.getRootUrl() + project.getUrl();
+        assertThat(url).isEqualTo(expectedUrl);
     }
 
     @Test
     public void testGetRunUrl_handlesNullSafely() {
         final String url = RunUrlProvider.getRunUrl(null);
         assertThat(url).isEmpty();
-    }
-
-    @Test
-    public void testGetPipelineUrl_fallsBackToParentAbsoluteUrl() throws Exception {
-        final RunWrapper runWrapper = mock(RunWrapper.class);
-        final Run run = mock(Run.class);
-        final Job job = mock(Job.class);
-
-        when(runWrapper.getRawBuild()).thenReturn(run);
-        when(run.getParent()).thenReturn(job);
-        when(job.getAbsoluteUrl())
-                .thenReturn("http://localhost:8080/job/test-repo/job/bugfix%252FSAL-1/");
-
-        final String url = RunUrlProvider.getPipelineUrl(runWrapper);
-
-        assertThat(url).isEqualTo("http://localhost:8080/job/test-repo/job/bugfix%252FSAL-1/");
     }
 
     @Test
